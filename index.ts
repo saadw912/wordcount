135c257ac9c45021b3b38ee960dd63f84012abb8
saadw912 <42513495+saadw912@users.noreply.github.com>
2023-01-09 23:51:37 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const sleep=()=>{
+   return new Promise((res,rej)=>{
+    setTimeout(res,1000)
+   })
+}
+async function welcome()
+{
+    const rainbowTitle=chalkAnimation.rainbow("------ Lets Start------");
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+async function askQuestion()
+{
+    let question= await inquirer.prompt([
+        {
+            type:"input",
+            name:"str",
+            message:chalk.red("please enter paragraph you want to convert: ")
+        }
+    ])
+    const word_arr=question.str.split(" ")
+   // console.log(word_arr);
+    console.log(`words in paragraph = ${word_arr.length}`);
+    const withOutSpace=question.str.replace(/ /g,"")
+    console.log(`characters in paragraph = ${withOutSpace.length}`);
+}
+
+async function againStart()
+{
+    do{
+         await askQuestion();
+        var playAgain=await inquirer.prompt({
+                    type:"input",
+                    name:"restart",
+                    message:chalk.red("Do you want to restart?")
+            
+    })
+    }while(playAgain.restart==='Y'||playAgain.restart==='y'||playAgain.restart==='yes'
+    ||playAgain.restart==='YES'||playAgain.restart==='Yes'||playAgain.restart===1)
+}
+ againStart();
